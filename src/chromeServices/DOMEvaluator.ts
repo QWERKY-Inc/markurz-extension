d28abf751c4de05ed71509c39b13349ee6712800
Fernando <fernand.veyrier@epitech.eu>
2023-06-01 17:34:09 +0900

    MKZ-8 Added token for session (WIP)

--- src/chromeServices/DOMEvaluator.ts
+++ src/chromeServices/DOMEvaluator.ts
@@ -1,12 +1,27 @@
 import { DOMMessage, DOMMessageResponse } from "../types";
 
 // Function called when a new message is received
-const messagesFromReactAppListener = (
+const messagesFromReactAppListener = async (
   msg: DOMMessage,
   sender: chrome.runtime.MessageSender,
   sendResponse: (response: DOMMessageResponse) => void
 ) => {
   console.log("[content.js]. Message received", msg);
+
+  if (msg.type === "GET_COOKIE") {
+    chrome.cookies.getAll(
+      { domain: "www.deepform.net", name: "__Secure-next-auth.session-token" },
+      (cookie) => {
+        console.log("get cookie", cookie);
+        if (cookie.length) {
+          sendResponse({ title: cookie[0].value || "", headlines: [] });
+        } else {
+          sendResponse({ title: "nothing" || "", headlines: [] });
+        }
+      }
+    );
+    return;
+  }
 
   const headlines = Array.from(document.getElementsByTagName<"h1">("h1")).map(
     (h1) => h1.innerText
--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,70 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import theme from "./theme";
 
-const app = document.createElement("div");
-app.id = "markurz-root";
-document.body.appendChild(app);
+/**
+ * Gets the cookie for the logged-in user.
+ * If there is one, inject the token in the page.
+ * If there is none, open a login page instead.
+ */
+function injectPlugin(token: string) {
+  chrome.tabs.query(
+    {
+      active: true,
+      currentWindow: true,
+    },
+    (tabs) => {
+      chrome.scripting?.executeScript({
+        target: { tabId: tabs[0].id || 0 },
+        args: [{ token }],
+        func: (args) => {
+          if (args.token) {
+            sessionStorage.setItem("markurz-token", args.token);
+          } else {
+            sessionStorage.removeItem("markurz-token");
+          }
+        },
+      });
+    }
+  );
+}
 
-const root = ReactDOM.createRoot(app);
-root.render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
-);
+if (chrome.cookies) {
+  chrome.cookies.onChanged.addListener((reason) => {
+    if (
+      reason.cookie.domain === "www.deepform.net" &&
+      reason.cookie.name === "__Secure-next-auth.session-token"
+    ) {
+      injectPlugin(reason.removed ? "" : reason.cookie.value);
+    }
+  });
+  chrome.cookies.getAll(
+    { domain: "www.deepform.net", name: "__Secure-next-auth.session-token" },
+    (cookie) => {
+      if (cookie.length) {
+        injectPlugin(cookie[0].value);
+      } else {
+        injectPlugin("");
+      }
+    }
+  );
+  // Inject the plugin only in the content
+} else {
+  const prevApp = document.getElementById("markurz-root");
+  // Make sure we don't have doubloons
+  if (prevApp) {
+    prevApp.outerHTML = "";
+  }
+  const app = document.createElement("div");
+  app.id = "markurz-root";
+  document.body.appendChild(app);
+
+  const root = ReactDOM.createRoot(app);
+  root.render(
+    <React.StrictMode>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </React.StrictMode>
+  );
+}
