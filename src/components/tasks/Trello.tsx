c4de517c14f45baac8bfe033843180e0c48ea1a0
Fernando <fernand.veyrier@epitech.eu>
2023-06-12 19:24:52 +0200

    MKZ-83 Trello sends label ids instead of text content

--- src/components/tasks/Trello.tsx
+++ src/components/tasks/Trello.tsx
@@ -1,10 +1,10 @@
 import { useLazyQuery, useQuery } from "@apollo/client";
 import { InfoOutlined } from "@mui/icons-material";
-import CheckBoxIcon from "@mui/icons-material/CheckBox";
-import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
 import {
-  Autocomplete,
+  Box,
   Checkbox,
+  Chip,
+  ListItemText,
   MenuItem,
   Stack,
   StackProps,
@@ -21,9 +21,6 @@ interface TrelloProps extends StackProps {
   userModuleId: string;
   highlightedText: string;
 }
-
-const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
-const checkedIcon = <CheckBoxIcon fontSize="small" />;
 
 const QUERY_TRELLO_WORKSPACES = graphql(/* GraphQL */ `
   query TrelloWorkspaces($userModuleId: ID!) {
@@ -65,7 +62,7 @@ const QUERY_TRELLO_LABELS = graphql(/* GraphQL */ `
       }
       elements {
         id
-        name
+        label: name
       }
     }
   }
@@ -176,34 +173,39 @@ const Trello = (props: TrelloProps) => {
       />
       <Controller
         render={({ field: { onChange, value, ...rest } }) => (
-          <Autocomplete
-            freeSolo
-            multiple
-            onChange={(e, data) => {
-              onChange(data);
-            }}
-            value={value || undefined}
+          <TextField
+            select
+            onChange={onChange}
+            value={value || []}
             {...rest}
-            options={
-              trelloLabels?.trelloLabels.elements?.map(
-                (element) => element.name
-              ) ?? []
-            }
-            renderOption={(props, option, { selected }) => (
-              <li {...props}>
+            label="Select Labels"
+            SelectProps={{
+              renderValue: (selected: any) => (
+                <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
+                  {selected.map((value: any) => (
+                    <Chip
+                      key={value}
+                      label={
+                        trelloLabels?.trelloLabels.elements?.find(
+                          (o) => o.id === value
+                        )?.label
+                      }
+                    />
+                  ))}
+                </Box>
+              ),
+              multiple: true,
+            }}
+          >
+            {trelloLabels?.trelloLabels.elements?.map((trelloLabel) => (
+              <MenuItem key={trelloLabel.id} value={trelloLabel.id}>
                 <Checkbox
-                  icon={icon}
-                  checkedIcon={checkedIcon}
-                  style={{ marginRight: 8 }}
-                  checked={selected}
+                  checked={Boolean(value && value.indexOf(trelloLabel.id) > -1)}
                 />
-                {option}
-              </li>
-            )}
-            renderInput={(params) => (
-              <TextField {...params} variant="outlined" label="Select Labels" />
-            )}
-          />
+                <ListItemText primary={trelloLabel.label} />
+              </MenuItem>
+            ))}
+          </TextField>
         )}
         name="element.labelIds"
         control={control}
