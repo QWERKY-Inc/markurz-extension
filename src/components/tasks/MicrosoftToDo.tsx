4af8fad4a67ec424f229ce7c057d5667a117bc05
Fernando <fernand.veyrier@epitech.eu>
2023-07-15 14:06:08 +0900

    MKZ-430 Added Microsoft To Do fields

--- src/components/tasks/MicrosoftToDo.tsx
+++ src/components/tasks/MicrosoftToDo.tsx
@@ -1,7 +1,16 @@
+import { useQuery } from "@apollo/client";
 import { InfoOutlined } from "@mui/icons-material";
-import { Stack, StackProps, TextField, Typography } from "@mui/material";
+import {
+  MenuItem,
+  Stack,
+  StackProps,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { DateTimePicker } from "@mui/x-date-pickers";
 import React from "react";
 import { Controller, useFormContext } from "react-hook-form";
+import { graphql } from "src/generated";
 import { MutationCreateMicrosoftTodoTaskArgs } from "src/generated/graphql";
 
 interface MicrosoftToDoProps extends StackProps {
@@ -9,17 +18,70 @@ interface MicrosoftToDoProps extends StackProps {
   highlightedText: string;
 }
 
+const QUERY_MICROSOFT_TODO_TASKS = graphql(/* GraphQL */ `
+  query MicrosoftTodoTaskLists($userModuleId: ID!, $take: Int, $skip: Int) {
+    microsoftTodoTaskLists(
+      userModuleId: $userModuleId
+      take: $take
+      skip: $skip
+    ) {
+      meta {
+        totalCount
+      }
+      elements {
+        id
+        displayName
+      }
+    }
+  }
+`);
+
+const QUERY_MICROSOFT_TODO_CATEGORIES = graphql(/* GraphQL */ `
+  query MicrosoftTodoCategories($userModuleId: ID!, $take: Int, $skip: Int) {
+    microsoftTodoCategories(
+      userModuleId: $userModuleId
+      take: $take
+      skip: $skip
+    ) {
+      elements {
+        id
+        displayName
+        color
+      }
+      meta {
+        totalCount
+      }
+    }
+  }
+`);
+
 const MicrosoftToDo = (props: MicrosoftToDoProps) => {
-  const { userModuleId, highlightedText } = props;
+  const { userModuleId, highlightedText, ...stackProps } = props;
   const { register, control } =
     useFormContext<MutationCreateMicrosoftTodoTaskArgs>();
+  const { data: dataTodoTasks, loading: loadingTasks } = useQuery(
+    QUERY_MICROSOFT_TODO_TASKS,
+    {
+      variables: {
+        userModuleId,
+      },
+    },
+  );
+  const { data: dataTodoCategories, loading: loadingCategories } = useQuery(
+    QUERY_MICROSOFT_TODO_CATEGORIES,
+    {
+      variables: {
+        userModuleId,
+      },
+    },
+  );
   register("userModuleId", { value: userModuleId });
 
   return (
-    <Stack spacing={3} {...props}>
+    <Stack spacing={3} {...stackProps}>
       <Typography display="flex" gap={1} alignItems="center">
         <InfoOutlined fontSize="small" />
-        Create an issue in MicrosoftToDo
+        Create an issue in Microsoft To Do
       </Typography>
       <Controller
         render={({ field }) => (
@@ -44,6 +106,38 @@ const MicrosoftToDo = (props: MicrosoftToDoProps) => {
         inputProps={{
           maxLength: 2000,
         }}
+      />
+      <Controller
+        render={({ field }) => (
+          <TextField {...field} select label="Select List" required>
+            {loadingTasks && <MenuItem disabled>Loading...</MenuItem>}
+            {!dataTodoTasks?.microsoftTodoTaskLists.elements?.length &&
+              !loadingTasks && <MenuItem disabled>No items</MenuItem>}
+            {dataTodoTasks?.microsoftTodoTaskLists.elements?.map((item) => (
+              <MenuItem key={item.id} value={item.id}>
+                {item.displayName}
+              </MenuItem>
+            ))}
+          </TextField>
+        )}
+        name="element.taskListId"
+        control={control}
+      />
+      <Typography color="text.secondary" sx={{ pt: 2 }}>
+        Additional Information (optional)
+      </Typography>
+      <TextField select label="Select Category">
+        {loadingCategories && <MenuItem disabled>Loading...</MenuItem>}
+        {!dataTodoCategories?.microsoftTodoCategories.elements?.length &&
+          !loadingCategories && <MenuItem disabled>No items</MenuItem>}
+        {dataTodoCategories?.microsoftTodoCategories.elements?.map((item) => (
+          <MenuItem key={item.id}>{item.displayName}</MenuItem>
+        ))}
+      </TextField>
+      <Controller
+        render={({ field }) => <DateTimePicker label="Due date" {...field} />}
+        name="element.dueDate"
+        control={control}
       />
     </Stack>
   );
