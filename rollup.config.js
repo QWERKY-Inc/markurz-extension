c1da522723a6a4dc923abbcab1e07699a25d33bc
Fernando <fernand.veyrier@epitech.eu>
2023-08-09 14:19:46 +0900

    MKZ-575 Added UMD export

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,31 @@ import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import { dts } from "rollup-plugin-dts";
+import { terser } from "rollup-plugin-terser";
+import babel from "@rollup/plugin-babel";
 
 const packageJson = require("./package.json");
 
 export default [
+  {
+    // UMD
+    input: "src/index.ts",
+    plugins: [
+      nodeResolve(),
+      babel({
+        babelHelpers: "bundled",
+      }),
+      terser(),
+    ],
+    output: {
+      file: `dist/${packageJson.name}.min.js`,
+      format: "umd",
+      name: "markurz",
+      esModule: false,
+      exports: "named",
+      sourcemap: true,
+    },
+  },
   {
     input: "src/index.ts",
     output: [
