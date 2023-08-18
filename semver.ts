07ab09932b351c37474f3b83abe0fa70e815c368
Fernando <fernand.veyrier@epitech.eu>
2023-08-18 10:47:32 +0900

    MKZ-633 Changed semver to match the package version

--- semver.ts
+++ semver.ts
@@ -1,14 +1,13 @@
-// @ts-ignore
 const fs = require("fs");
-const semverInc = require("semver/functions/inc");
+const manifest = require("./public/manifest.json");
+const packageJson = require("./package.json");
 
 const FILE_PATH = "./public/manifest.json";
 
 try {
-  const jsonData = JSON.parse(fs.readFileSync(FILE_PATH, "utf-8"));
-  jsonData["version"] = semverInc(jsonData["version"], "patch");
-  console.log(`Semver: increased current version to [${jsonData["version"]}]`);
-  fs.writeFileSync(FILE_PATH, JSON.stringify(jsonData, null, 2));
+  manifest["version"] = packageJson["version"];
+  console.log(`Semver: increased current version to [${manifest["version"]}]`);
+  fs.writeFileSync(FILE_PATH, JSON.stringify(manifest, null, 2));
 } catch (e) {
   console.error(e);
 }
